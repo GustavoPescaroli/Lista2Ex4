fb955b80a7989f5387349e8b097dbf7393395b6d
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-05-03 20:01:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex4
+{
+
+    
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int b;
+                int h;
+                int a;
+
+                Console.Write("insira o Valor da Base: ");
+                b = int.Parse(Console.ReadLine());
+
+                Console.Write("insira o Valor da altura: ");
+                h = int.Parse(Console.ReadLine());
+
+                a = b * h;
+                 Console.WriteLine("valor da area {0}", a);
+
+                if (a > 100)
+                    Console.WriteLine("Terreno Grande.");
+
+
+                
+
+                
+                
+
+
+
+                
+            }
+        }
+    
+}
+    
+
